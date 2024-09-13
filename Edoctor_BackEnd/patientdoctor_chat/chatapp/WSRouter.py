63334234a6ff9c34d11e235f48693b99a48264aa
aivan2798 <cot2we@gmail.com>
2024-09-13 06:16:10 +0300

    added appointment reply

--- Edoctor_BackEnd/patientdoctor_chat/chatapp/WSRouter.py
+++ Edoctor_BackEnd/patientdoctor_chat/chatapp/WSRouter.py
@@ -436,7 +436,8 @@ class ChatRouter:
              }
         channel_name = assigned_user.channel_name
         appointment_json = WSResponseMdl(200,"appointment","New Appointment",appointment_dic)
-        await self.chat_channel_layer.send(self.channel_name,{"type": "raw_chat_message","text":appointment.serial()})
+        await self.chat_channel_layer.send(self.chat_to_channel,{"type": "raw_chat_message","text":appointment.serial()})
+        await self.chat_channel_layer.send(channel_name,{"type": "raw_chat_message","text":appointment.serial()})
         #pass
 
     """function that handles lab test"""
